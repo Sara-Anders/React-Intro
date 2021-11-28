aeb74e8e9ec405ac3cd2d60d806ed3d319aa2d39
Sara Anderson <89275841+Sara-Anders@users.noreply.github.com>
2021-11-28 01:36:28 +0000

    style font changes, had <strong> tag in wrong place, moved it to right place

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-/* Students: Please use this week's project for Week 15: Assignment 13: Basic Mobile App. 
-     You will need to replace the contents of this JavaScript file with your own work, 
-     and create any other files, if any, required for the assignment.
-     When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+
 'use strict';
 
 // define dataset as array in a variable
 const info = [
-  { title: <strong>'Week 3 JS Quiz'</strong>, detail: 'Learning about Events and Objects of Javascript, created a quiz that has the user input an answer to a question, using to create an interactive experiance using logic and control structures. ', image_trail: 'images/quiz.png'},
+  { title: 'Week 3 JS Quiz', detail: 'Learned about Events and Objects of Javascript, created a quiz that has the user input an answer to a question, using to create an interactive experiance using logic and control structures. ', image_trail: 'images/quiz.png'},
 
-  { title: <strong>'Week 7 JQuery Plug-ins'</strong>, detail: 'Learned to create jQuery plugins using object-oriented JavaScript code. Also, learned to research, evaluate and integrate multiple jQuery plugins to accomplish desired user experience.', image_trail: 'images/plugins.png' },
+  { title: 'Week 7 JQuery Plug-ins', detail: 'Learned to create jQuery plugins using object-oriented JavaScript code. Also, learned to research, evaluate and integrate multiple jQuery plugins to accomplish desired user experience.', image_trail: 'images/plugins.png' },
 
-  { title: <strong>'Mid-Term Project Google Maps App'</strong>, detail: 'Worked with dynamic data loaded from external JSON files. Worked with asynchronous events and JavaScript callback function. Created mobile-ready responsive interfaces using JavaScript, HTML and CSS.Respond to user events with function-based code blocks.', image_trail: 'images/midterm.png' },
+  { title:'Mid-Term Project Google Maps App', detail: 'Worked with dynamic data loaded from external JSON files. Worked with asynchronous events and JavaScript callback function. Created mobile-ready responsive interfaces using JavaScript, HTML and CSS.Respond to user events with function-based code blocks.', image_trail: 'images/midterm.png' },
 
-  { title: <strong>'Week 11 Basic Data Visualizations'</strong>, detail: 'Developed data visualizations using external JSON data. Learned how JavaScript can command the browser to create visual graphics in HTML5 canvas elements.', image_trail: 'images/graph.png' },
+  { title: 'Week 11 Basic Data Visualizations', detail: 'Developed data visualizations using external JSON data. Learned how JavaScript can command the browser to create visual graphics in HTML5 canvas elements.', image_trail: 'images/graph.png' },
 
-  { title: <strong>'Week 12 Advanced Data Visualizations'</strong>, detail: 'Created a data visualization using the D3 library that supports user interaction using hover and click interaction elements, uses SVG elements and loads data from an external JSON file. ', image_trail: 'images/graph2.png' },
+  { title: 'Week 12 Advanced Data Visualizations', detail: 'Created a data visualization using the D3 library that supports user interaction using hover and click interaction elements, uses SVG elements and loads data from an external JSON file. ', image_trail: 'images/graph2.png' },
 
-   { title: <strong>'Week 14 Basic Game Development'</strong>, detail: 'Developed a highly interactive user experiences using the Phaser JavaScript library. Learned how JavaScript can command the browser to create games. ', image_trail: 'images/game.png' }
+   { title:'Week 14 Basic Game Development', detail: 'Developed a highly interactive user experiences using the Phaser JavaScript library. Learned how JavaScript can command the browser to create games. ', image_trail: 'images/game.png' }
   
   
 ];
@@ -34,7 +31,7 @@ return <div>
         //copy current array valur into item and pass to func 
         (item, index) => (
           <li key={index}>
-            {item.title}
+            <strong>{item.title}</strong>
             <br/>
             {item.detail}
              <br/>
@@ -49,6 +46,6 @@ return <div>
 
 //put it onto the page with render
 ReactDOM.render(
-  <TaskList list={info} text1="A compilation of 50C Intro to Javascript Projects" />,
+  <TaskList list={info} text1="A Compilation of 50C Intro to Javascript Projects" />,
   document.getElementById('myProject')
 );
